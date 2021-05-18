be3f778451075ee82349fe00b4f9decedfef93df
Jan Przychodniak <jan.przychodniak@gmail.com>
2021-05-18 16:39:31 +0200

    Indekser generujący kolejne klucze główne

---
+++ DesRainbowTable/indexer.cu
@@ -0,0 +1,10 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "indexer.cuh"
+
+void getKeyForIndex(unsigned long index, unsigned short numBytes, unsigned char* outBytes) {
+	for (short i = 0; i < numBytes; i++) {
+		outBytes[i] = index % 256;
+		index /= 256;
+	}
+}
---
+++ DesRainbowTable/main.cu
@@ -0,0 +1,21 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "indexer.cuh"
+
+int main() {
+	unsigned char key[8];
+	for (int i = 0; i < 5000; i += 1) {
+		printf("Klucz[%d]: ", i);
+		getKeyForIndex(i, 8, key);
+		for (int j = 0; j < 8; j++) {
+			printf("%d ", key[j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
