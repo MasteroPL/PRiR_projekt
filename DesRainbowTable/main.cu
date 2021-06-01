93b3bde586b0d3db6ca0a30c41aeeecf5598cea3
Jan Przychodniak <jan.przychodniak@gmail.com>
2021-06-01 15:51:31 +0200

    Alokacja pamięci dla tablic tęczowych (z uwzględnieniem maksymalnej wielkości alokacji)

--- DesRainbowTable/main.cu
+++ DesRainbowTable/main.cu
@@ -1,5 +1,6 @@
 ﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include "rainbow_table.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +8,7 @@
 #include "indexer.cuh"
 
 int main() {
-	unsigned char key[8];
+	/*unsigned char key[8];
 	for (int i = 0; i < 5000; i += 1) {
 		printf("Klucz[%d]: ", i);
 		getKeyForIndex(i, 8, key);
@@ -15,7 +16,39 @@ int main() {
 			printf("%d ", key[j]);
 		}
 		printf("\n");
+	}*/
+
+	rainbow_table_t* t = RainbowTable_allocate(8, 8, 20);
+
+	printf("\n------------\n");
+
+	for (int i = 0; i < 20; i++) {
+		printf("%p\n", t->nodes[i].key);
+		printf("%p\n", t->nodes[i].encoded_password);
+
+		t->nodes[i].key[0] = (char)(65 + i);
+		t->nodes[i].encoded_password[0] = (char)(97 + i);
+		t->nodes[i].key[7] = '\0';
+		t->nodes[i].encoded_password[7] = '\0';
+
+		printf("%p\n", t->nodes[i].key);
+		printf("%p\n", t->nodes[i].encoded_password);
+		printf("\n------------\n");
+
+		for (int j = 0; j < 6; j++) {
+			t->nodes[i].key[1 + j] = (char)(48 + j);
+			t->nodes[i].encoded_password[1 + j] = (char)(48 + j);
+		}
 	}
+
+	for (int i = 0; i < 20; i++) {
+		printf("%i: %s | %s\n", i, t->nodes[i].key, t->nodes[i].encoded_password);
+	}
+	/*
+
+	RainbowTable_write_to_file(t, "test.txt");*/
+
+	RainbowTable_free(t);
 
 	return 0;
 }
---
+++ DesRainbowTable/rainbow_table.h
@@ -0,0 +1,40 @@
+
+#define MAX_MALLOC_SIZE 16711568
+
+typedef struct RainbowTableNode {
+	char* key;
+	char* encoded_password;
+} rainbow_table_node_t;
+
+typedef struct RainbowTable {
+	short key_size; // Tu zawsze bedzie 8
+	short encoded_password_size; // Tutaj w zale¿noœci od has³a sta³a wartoœæ dla s³ownika
+	int nodes_size;
+
+	int _num_of_refs;
+	char** _nodes_data_ref;
+	int* _ref_sizes;
+
+	rainbow_table_node_t* nodes;
+} rainbow_table_t;
+
+/// <summary>
+/// Alokuje pamiec dla tablicy teczowej o wybranym rozmiarze
+/// </summary>
+/// <param name="key_size">Rozmiar (w bajtach) kluczy (zawsze 8)</param>
+/// <param name="encoded_password_size">Rozmiar (w bajtach) zaszyfrowanej treœci hasla</param>
+/// <param name="num_of_entries">Liczba wpisow do zaalokowania</param>
+/// <returns>Wskaznik na zaalokowan¹ pamiec</returns>
+rainbow_table_t* RainbowTable_allocate(short key_size, short encoded_password_size, int num_of_entries);
+
+/// <summary>
+/// Czyœci pamiêæ wybranej tablicy
+/// </summary>
+void RainbowTable_free(rainbow_table_t* self);
+
+/// <summary>
+/// Wypisuje tablicê do pliku
+/// </summary>
+/// <param name="filename">Nazwa pliku do wypisania</param>
+/// <returns>0 - zapis udany, 1 - b³¹d otwarcia pliku, 2 - b³¹d zapisu</returns>
+int RainbowTable_write_to_file(rainbow_table_t* self, const char* filename);
